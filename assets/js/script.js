d01b9bffa3f3b4f8a306d20626c6c9864f5d2f14
habib-tec <habib.qtec@gmail.com>
2025-05-04 18:12:32 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,7 +122,6 @@ const formBtn = document.querySelector("[data-form-btn]");
 
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
-  
   formInputs[i].addEventListener("input", function () {
 
     // check form validation
@@ -133,6 +132,10 @@ for (let i = 0; i < formInputs.length; i++) {
     }
 
   });
+}
+
+const successMessage=()=>{
+  alert("Masage Sent Sucessfully!")
 }
 
 
@@ -158,37 +161,3 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
-
-
-
-// ##########################
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
